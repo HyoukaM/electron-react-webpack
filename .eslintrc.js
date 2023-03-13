1dd3ab53b7edec8ada15489b25167522a6a1b412
hyoukam <838611931@qq.com>
2023-03-13 14:38:18 +0800

    feat: 配置eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,38 @@
 module.exports = {
-  extends: 'erb',
-  rules: {
-    'import/no-extraneous-dependencies': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': 'off',
-    'import/extensions': 'off',
-    'import/no-unresolved': 'off',
-    'import/no-import-module-exports': 'off',
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
-    createDefaultProgram: true,
-  },
-  settings: {
-    'import/resolver': {
-      node: {},
-      webpack: {
-        config: require.resolve('./.erb/configs/webpack.config.eslint.ts'),
-      },
-      typescript: {},
+    extends: 'erb',
+    rules: {
+        'import/no-extraneous-dependencies': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-filename-extension': 'off',
+        'import/extensions': 'off',
+        'import/no-unresolved': 'off',
+        'import/no-import-module-exports': 'off',
+        'import/no-duplicates': 'off',
+        'import/no-self-import': 'off',
+        'import/no-useless-path-segments': 'off',
+        'import/order': 'off',
+        'import/no-cycle': 'off',
+        'import/no-relative-packages': 'off',
+        'import/no-named-as-default': 'off',
+        'import/no-named-as-default-member': 'off',
     },
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        createDefaultProgram: true,
     },
-  },
+    settings: {
+        'import/resolver': {
+            node: {},
+            webpack: {
+                config: require.resolve('./.erb/configs/webpack.config.eslint.ts'),
+            },
+            typescript: {},
+        },
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+    },
 };
