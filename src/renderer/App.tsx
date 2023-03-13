1dcf557c942bee7cc0d04949f0c94887623a947f
hyoukam <838611931@qq.com>
2023-03-13 15:45:21 +0800

    feat: 添加测试文件

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import a from './assets/audio/t-rex-roar.mp3';
+import s from 'renderer/assets/test.module.less';
+// import s from 'src/renderer/assets/test.module.less';
+
+console.log(s);
+// import * as e from 'electron';
+//
+// console.log(e);
+//
+console.log(window.electron);
 
 function Hello() {
-  return (
-    <div className="test">
-      <audio src={a} controls loop muted>
-        <source src={a} type="audio/mp3" />
-      </audio>
-    </div>
-  );
+    return (
+        <div className={s.module}>
+            <audio src={a} controls loop muted>
+                <source src={a} type="audio/mp3" />
+            </audio>
+        </div>
+    );
 }
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Hello />} />
+            </Routes>
+        </Router>
+    );
 }
