0e3bc1dd5442b44e855efa3ce094fba123e1d949
hyoukam <838611931@qq.com>
2023-03-13 15:27:15 +0800

    feat: 添加备注

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,27 +1,31 @@
-// Disable no-unused-vars, broken for spread args
-/* eslint no-unused-vars: off */
-import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
+/**
+ * TODO 这边是向全局（windows）注入electron方法
+ * @link https://www.electronjs.org/zh/docs/latest/tutorial/process-model#preload-%E8%84%9A%E6%9C%AC
+ */
+import { contextBridge, ipcRenderer, IpcRendererEvent, clipboard } from 'electron';
 
 export type Channels = 'ipc-example';
 
 const electronHandler = {
-  ipcRenderer: {
-    sendMessage(channel: Channels, args: unknown[]) {
-      ipcRenderer.send(channel, args);
-    },
-    on(channel: Channels, func: (...args: unknown[]) => void) {
-      const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
-        func(...args);
-      ipcRenderer.on(channel, subscription);
+    ipcRenderer: {
+        sendMessage(channel: Channels, args: unknown[]) {
+            ipcRenderer.send(channel, args);
+        },
+        on(channel: Channels, func: (...args: unknown[]) => void) {
+            const subscription = (_event: IpcRendererEvent, ...args: unknown[]) => func(...args);
+            ipcRenderer.on(channel, subscription);
 
-      return () => {
-        ipcRenderer.removeListener(channel, subscription);
-      };
+            return () => {
+                ipcRenderer.removeListener(channel, subscription);
+            };
+        },
+        once(channel: Channels, func: (...args: unknown[]) => void) {
+            ipcRenderer.once(channel, (_event, ...args) => func(...args));
+        },
     },
-    once(channel: Channels, func: (...args: unknown[]) => void) {
-      ipcRenderer.once(channel, (_event, ...args) => func(...args));
+    clipboard: {
+        ...clipboard,
     },
-  },
 };
 
 contextBridge.exposeInMainWorld('electron', electronHandler);
