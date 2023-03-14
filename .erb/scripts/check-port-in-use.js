b7497347cd1cf548bc06fc0b464b9527c3d5cc4b
hyoukam <838611931@qq.com>
2023-03-14 09:53:24 +0800

    feat: 格式化

--- .erb/scripts/check-port-in-use.js
+++ .erb/scripts/check-port-in-use.js
@@ -4,13 +4,9 @@ import detectPort from 'detect-port';
 const port = process.env.PORT || '2580';
 
 detectPort(port, (err, availablePort) => {
-  if (port !== String(availablePort)) {
-    throw new Error(
-      chalk.whiteBright.bgRed.bold(
-        `端口 "${port}" 已经被占用. 请更换端口后重新启动`
-      )
-    );
-  } else {
-    process.exit(0);
-  }
+    if (port !== String(availablePort)) {
+        throw new Error(chalk.whiteBright.bgRed.bold(`端口 "${port}" 已经被占用. 请更换端口后重新启动`));
+    } else {
+        process.exit(0);
+    }
 });
