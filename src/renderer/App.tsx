66cc14219ad58f9c8e3c033a7e0dba8c386f7249
hyoukam <838611931@qq.com>
2023-03-14 09:56:13 +0800

    feat: 删除不属于基础框架的代码

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,24 +1,8 @@
 import React from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import a from './assets/audio/t-rex-roar.mp3';
-import s from 'renderer/assets/test.module.less';
-// import s from 'src/renderer/assets/test.module.less';
-
-console.log(s);
-// import * as e from 'electron';
-//
-// console.log(e);
-//
-console.log(window.electron);
 
 function Hello() {
-    return (
-        <div className={s.module}>
-            <audio src={a} controls loop muted>
-                <source src={a} type="audio/mp3" />
-            </audio>
-        </div>
-    );
+    return <div className="test">123</div>;
 }
 
 export default function App() {
